a4f3edd0093320ec2ff6d44e0f87a7ee9b3e0e61
tarun <tarun.baloch@autoninja.in>
2016-02-16 19:01:06 +0530

    remove print

--- mixpanel.py
+++ mixpanel.py
@@ -3,8 +3,9 @@ import pandas as pd
 #read the excel file
 file_name='mixpanel analysis dealers.xlsx'
 def read_excel_file(filename=file_name):
-    print filename
     return pd.read_excel(io=filename, sheetname=None)
+
+def analytics_function():
 
 if __name__ == '__main__':
     read_excel_file()
